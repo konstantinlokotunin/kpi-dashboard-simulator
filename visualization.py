604054a0785f333f224b22537ff4973a135b9197
k-lokotunin <konstantin.lokotunin@sciencespo.fr>
2026-03-31 20:39:32 +0200

    Add function to plot KPIs using matplotlib

--- visualization.py
+++ visualization.py
@@ -1,1 +1,22 @@
+import matplotlib.pyplot as plt
 
+def plot_kpis(months, revenue, costs, profit):
+    fig, axs = plt.subplots(3, 1, figsize=(8, 10), sharex=True)
+
+    axs[0].plot(months, revenue)
+    axs[0].set_title("Revenue")
+
+    axs[1].plot(months, costs)
+    axs[1].set_title("Costs")
+
+    axs[2].plot(months, profit)
+    axs[2].set_title("Profit")
+
+    for ax in axs:
+        ax.set_xlabel("Month")
+        ax.set_ylabel("USD")
+
+    fig.suptitle("Company KPI Overview")
+
+    plt.tight_layout()
+    plt.show()
