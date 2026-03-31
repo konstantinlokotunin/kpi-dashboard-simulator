4d1bbe5661a3aa55e6127d1e254b1b6b601a22df
k-lokotunin <konstantin.lokotunin@sciencespo.fr>
2026-03-31 20:30:02 +0200

    Add functions for profit calculation and statistics

--- analysis.py
+++ analysis.py
@@ -1,1 +1,19 @@
+def calculate_profit(revenue, costs):
+    profit = []
+    
+    for i in range(len(revenue)):
+        profit.append(revenue[i] - costs[i])
+    
+    return profit
 
+
+def calculate_average(values):
+    return sum(values) / len(values)
+
+
+def find_max(values):
+    return max(values)
+
+
+def find_min(values):
+    return min(values)
