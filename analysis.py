4882c75738244449c68ebbbb770c7cdff34b8d77
k-lokotunin <konstantin.lokotunin@sciencespo.fr>
2026-04-01 12:56:55 +0200

    Refactor calculations to use NumPy
    Refactor profit, growth rate, average, max, and min calculations to use NumPy for improved performance and readability.

--- analysis.py
+++ analysis.py
@@ -1,19 +1,23 @@
-def calculate_profit(revenue, costs):
-    profit = []
-    
-    for i in range(len(revenue)):
-        profit.append(revenue[i] - costs[i])
-    
-    return profit
+import numpy as np
 
+def calculate_profit(revenue, costs):
+    revenue_arr = np.array(revenue)
+    costs_arr = np.array(costs)
+    
+    return revenue_arr - costs_arr
+
+def calculate_growth_rate(values):
+    values_arr = np.array(values)
+    
+    growth_rates = (values_arr[1:] - values_arr[:-1]) / values_arr[:-1]
+    
+    return growth_rates
 
 def calculate_average(values):
-    return sum(values) / len(values)
-
+    return np.mean(values)
 
 def find_max(values):
-    return max(values)
-
+    return np.max(values)
 
 def find_min(values):
-    return min(values)
+    return np.min(values)
